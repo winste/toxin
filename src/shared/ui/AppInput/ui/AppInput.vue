8d5dc33e03d150d71537ba5acec9ad122d0f9117
Stepanova Viktoria <stepanovavkt@gmail.com>
2024-03-06 20:11:52 +0700

    refactor: удалит лишний код

--- src/shared/ui/AppInput/ui/AppInput.vue
+++ src/shared/ui/AppInput/ui/AppInput.vue
@@ -4,14 +4,6 @@ const props = defineProps({
     type: String,
     default: 'text',
   },
-  label: {
-    type: String,
-    default: '',
-  },
-  title: {
-    type: String,
-    default: '',
-  },
   placeholder: {
     type: String,
     default: '',
@@ -28,52 +20,18 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="app-input">
-    <span class="title">
-      <label class="label" v-text="props.label" />
-      <span v-text="props.title" />
-    </span>
-    <input
-      :type="props.type"
-      :placeholder="props.placeholder"
-      :value="props.value"
-      :class="[ 'field', {' _active' : props.isActive} ]"
-    >
-  </div>
+  <input
+    :type="props.type"
+    :placeholder="props.placeholder"
+    :value="props.value"
+    :class="[ 'app-input field', {' _active' : props.isActive} ]"
+  >
 </template>
 
 <style scoped lang="scss">
-.app-input {
-  max-width: 320px;
-
-  .title {
-    display: flex;
-    justify-content: space-between;
-    margin-bottom: 8px;
-    font-size: 12px;
-    text-transform: uppercase;
-  }
-
-  .label {
-    font-weight: 700;
-  }
-
-  .field {
-    width: 100%;
-    height: 44px;
-    padding: 15px 13px;
-    border: 1px solid $dark25;
-    border-radius: 4px;
-
-    &::placeholder {
-      color: $dark25;
-      font-size: 14px;
-    }
-
-    &:active,
-    &._active {
-      border: 1px solid $dark50;
-    }
+.field {
+  &._active {
+    border: 1px solid $dark50;
   }
 }
 </style>
